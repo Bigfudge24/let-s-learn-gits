0aeb4672a7af6dea6223d801e7ad53804941857c
beugfudge <tomoyaa@tutamail.com>
2023-01-31 14:09:20 +0530

    adding message

--- Div.java
+++ Div.java
@@ -1,3 +1,8 @@
-public class Div {
-    
+public class Div{
+    public static void main(String[] args) {
+        int a=5;
+        int b=9;
+        System.out.println("This is the sum of two numbers"+a/b);
+    }
 }
+
--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(String[] args) {
         int a=5;
         int b=9;
-        System.out.println(a+b);
+        System.out.println("This is the sum of two numbers"+a+b);
     }
 }
 
